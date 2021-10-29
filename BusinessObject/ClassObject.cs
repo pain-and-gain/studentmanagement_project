3e2dfb9666d735141d4e476bf42eef7f5b20d4ce
thanhnguyen1092001 <th.tiger69@gmail.com>
2021-10-29 09:54:08 +0700

    Add class object and Repository

---
+++ BusinessObject/ClassObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class ClassObject
+    {
+        public string classID;
+        public string studentID;
+        public string className;
+        public string userID;
+        public string schoolYear;
+    }
+}
